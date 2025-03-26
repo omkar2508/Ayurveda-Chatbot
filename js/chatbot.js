3d9deb7b50a5c2291c7ca4c3860b36b652a78433
Omkar Dattatraya Babar <omkarbabar2508@gmail.com>
2025-03-26 13:19:41 +0530

    Project Added

---
+++ js/chatbot.js
@@ -0,0 +1,11 @@
+// Botpress Chatbot Integration
+window.botpressWebChat.init({
+    "botId": "your-bot-id",
+    "host": "https://cdn.botpress.cloud/webchat/v2.2/shareable.html",
+    "configUrl": "https://files.bpcontent.cloud/2024/10/21/05/20241021054921-7L32P58C.json",
+    "showCloseButton": true,
+    "showMinimizeButton": true,
+    "enableConversationDeletion": false,
+    "theme": "light"
+  });
+  
